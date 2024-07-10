7e5fc1d93c555aee74c0932aa6a99e7c29fa1dd7
Dhruv27Mishra <117510967+Dhruv27Mishra@users.noreply.github.com>
2024-07-11 03:52:41 +0530

    add

---
+++ Daily-Codes/Day-37.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int minOperations(String[] logs) {
+        int c=0;
+        for(String log: logs)
+        {
+            
+            if (log.equals("../"))
+                {if(c>0)c--;}
+            else if(log.equals("./"))
+              continue;
+            else
+            c++;
+            
+            
+        }
+        return Math.max(c,0);
+        
+    }
+}
+#self
